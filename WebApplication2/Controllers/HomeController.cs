c66ae6ade2ee11a6ce83073076f2649c93daa58e
faynuy <oleksiv1989@gmail.com>
2021-02-19 14:20:20 +0200

    add education application first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            int temp = 10;
+
+            List<User> users = new List<User>();
+
+            users.Add(new User
+            {
+                Id = 1,
+                Name = "Vova",
+                SurName = "Vovanovich",
+                Balance = 100,
+                Created = DateTime.Now,
+            });
+
+            users.Add(new User
+            {
+                Id = 2,
+                Name = "Ivan",
+                SurName = "Ivanovich",
+                Balance = 10000,
+                Created = DateTime.Now,
+            });
+
+            users.Add(new User
+            {
+                Id = 3,
+                Name = "Seva",
+                SurName = "Sevovich",
+                Balance = 777,
+                Created = DateTime.Now,
+            });
+
+            User testUser = new User();
+
+            testUser.Id = 4;
+            testUser.Name = "Vasa";
+            testUser.SurName = "Vasovich";
+            testUser.Balance = 1234;
+            testUser.Created = DateTime.Now;
+
+            users.Add(testUser);
+
+
+            return View(users);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public double Balance { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
